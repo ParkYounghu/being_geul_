2527f8f6bc47518a5216955e6d13ec681dada48d
ParkYounghu <dudgn7715@gmail.com>
2025-12-02 15:03:24 +0900

    asgfdsag

--- being_geul_/src/main.jsx
+++ being_geul_/src/main.jsx
@@ -1,13 +1,13 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // ★ 이 줄 추가!
+import { BrowserRouter } from 'react-router-dom' // ★ 이 줄이 꼭 있어야 함!
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* App을 BrowserRouter로 감싸야 페이지 이동이 작동합니다 */}
+    {/* ★ App 컴포넌트를 BrowserRouter로 감싸야 페이지 이동이 작동함 */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
